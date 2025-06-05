c2d20de299efb598e35921aefdda0a44181a4faa
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-06-06 02:08:33 +0300

    feature: created order module

--- api/src/modules/api.module.ts
+++ api/src/modules/api.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [AuthModule]
+  imports: [AuthModule, OrderModule],
 })
 export class ApiModule {}
