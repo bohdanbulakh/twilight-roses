47defbfa992ba94d1c961c6e40a7ab7ae11c272c
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-06-06 02:30:10 +0300

    feature: added crud in order service

--- api/src/modules/order/order.service.ts
+++ api/src/modules/order/order.service.ts
@@ -1,7 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { OrderRepository } from '../../database/repositories/order.repository';
+import { CreateOrderDTO } from '@twilight-roses/utils';
 
 @Injectable()
 export class OrderService {
   constructor (private readonly orderRepository: OrderRepository) {}
+
+  getAll () {
+    return this.orderRepository.findMany({})
+  }
+
+  getById (id: string) {
+    return this.orderRepository.findById(id);
+  }
+
+  create (body: CreateOrderDTO) {
+    return this.orderRepository.create(body);
+  }
+
+  deleteById (id: string) {
+    return this.orderRepository.deleteById(id)
+  }
 }
