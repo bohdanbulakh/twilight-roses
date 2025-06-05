6d1c287b2b5eaf9dd7cff7caf6f656b859c2b728
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-06-06 02:35:22 +0300

    feature: implemented CRUD for orders

--- api/src/modules/order/order.controller.ts
+++ api/src/modules/order/order.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { OrderService } from './order.service';
+import { CreateOrderDTO } from '@twilight-roses/utils';
 
 @Controller('/orders')
 export class OrderController {
   constructor (private readonly orderService: OrderService) {}
+
+  @Get()
+  getAll () {
+    return this.orderService.getAll();
+  }
+
+  @Get('/:id')
+  getById (@Param('id') id: string) {
+    return this.orderService.getById(id);
+  }
+
+  @Post()
+  create (@Body() body: CreateOrderDTO) {
+    return this.orderService.create(body);
+  }
+
+  @Delete('/:id')
+  deleteById (@Param('id') id: string) {
+    return this.orderService.deleteById(id);
+  }
 }
