40be0f1849eb5070e53b806195ef4c127ff8be7c
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-06-07 01:11:20 +0300

    feature: created endpoints for products

--- api/src/modules/product/product.controller.ts
+++ api/src/modules/product/product.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
-import { ProductRepository } from '../../database/repositories/product.repository';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CreateProductDTO, UpdateProductDTO } from '@twilight-roses/utils/requests';
+import { ProductService } from './product.service';
 
 @Controller('products')
 export class ProductController {
-  constructor (private readonly productService: ProductRepository) {}
+  constructor (private readonly productService: ProductService) {}
+
+  @Get()
+  getAll () {
+    return this.productService.getAll();
+  }
+
+  @Get(':id')
+  getById (@Param('id') id: string) {
+    return this.productService.getById(id);
+  }
+
+  @Post()
+  create (@Body() data: CreateProductDTO) {
+    return this.productService.create(data);
+  }
+
+  @Post(':id')
+  updateById (
+    @Param('id') id: string,
+    @Body() data: UpdateProductDTO,
+  ) {
+    return this.productService.updateById(id, data);
+  }
+
+  @Delete(':id')
+  deleteById (@Param('id') id: string) {
+    return this.productService.deleteById(id);
+  }
 }
