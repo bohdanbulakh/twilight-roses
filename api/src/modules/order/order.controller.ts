7f1725b656903316d41a24fa51a8804d19cbe2e1
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-06-06 13:10:07 +0300

    feature: added access guards and swagger docs to endpoints of order controller

--- api/src/modules/order/order.controller.ts
+++ api/src/modules/order/order.controller.ts
@@ -2,27 +2,48 @@ import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderByIdPipe } from '../../common/pipes/order-by-id.pipe';
 import { GetUser } from '../../common/decorators/get-user.decorator';
+import { ApiEndpoint } from 'src/common/decorators/api-endpoint.decorator';
+import { OrderDocumentation } from '../../common/documentation/modules/order';
+import { AccessGuard } from '../../common/guards/auth/access.guard';
 
 @Controller('/orders')
 export class OrderController {
   constructor (private readonly orderService: OrderService) {}
 
   @Get()
+  @ApiEndpoint({
+    summary: 'Get all orders',
+    documentation: OrderDocumentation.GET_ALL,
+  })
   getAll () {
     return this.orderService.getAll();
   }
 
   @Get('/:id')
+  @ApiEndpoint({
+    summary: 'Get order by id',
+    documentation: OrderDocumentation.GET_BY_ID,
+  })
   getById (@Param('id', OrderByIdPipe) id: string) {
     return this.orderService.getById(id);
   }
 
   @Post()
+  @ApiEndpoint({
+    summary: 'Create new order',
+    documentation: OrderDocumentation.CREATE,
+    guards: AccessGuard,
+  })
   create (@GetUser('id') userId: string) {
     return this.orderService.create({ userId });
   }
 
   @Delete('/:id')
+  @ApiEndpoint({
+    summary: 'Delete order by id',
+    documentation: OrderDocumentation.DELETE_BY_ID,
+    guards: AccessGuard,
+  })
   deleteById (@Param('id', OrderByIdPipe) id: string) {
     return this.orderService.deleteById(id);
   }
