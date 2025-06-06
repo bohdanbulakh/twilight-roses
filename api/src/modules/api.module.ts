7c50ebe8f81e3c55de2a8fe0a71cf9583df674a2
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-06-07 00:19:38 +0300

    feature: created product module

--- api/src/modules/api.module.ts
+++ api/src/modules/api.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [AuthModule, OrderModule],
+  imports: [AuthModule, OrderModule, ProductModule],
 })
 export class ApiModule {}
