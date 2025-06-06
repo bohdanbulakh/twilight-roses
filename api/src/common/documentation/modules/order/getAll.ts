ce2cde601e9f8544f01e5b62054d6672fd5a3046
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-06-07 00:10:55 +0300

    bugfix: fixed orders response type

--- api/src/common/documentation/modules/order/getAll.ts
+++ api/src/common/documentation/modules/order/getAll.ts
@@ -1,8 +1,8 @@
-import { OrderResponse } from '@twilight-roses/utils/responses';
+import { OrdersResponse } from '@twilight-roses/utils/responses';
 import { ApiDocumentationParams } from '../../types/api-documentation-params.type';
 
 export const GetAll: ApiDocumentationParams = {
   ok: {
-    type: [OrderResponse],
+    type: OrdersResponse,
   },
 };
